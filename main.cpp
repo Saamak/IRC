a780454b8170b35d717b1469b803518e942ba9bf
Your Name <you@example.com>
2025-02-03 18:27:17 +0100

    passs

--- main.cpp
+++ main.cpp
@@ -1,18 +1,23 @@
 #include "server.hpp"
+#include <cstring> // Pour utiliser strcmp
+
+const char* SECURE_PASSWD = "D789";
 
 int check_input(int ac, char **av)
 {
-    if (ac != 2) {
-        std::cerr << "Usage: " << av[0] << " <port>" << std::endl;
+    if (ac != 3) {
+        std::cerr << "Usage: " << av[0] << " <port> <password>" << std::endl;
         return 1;
     }
-    
+    if (strcmp(av[2], SECURE_PASSWD) != 0) {
+        std::cerr << "Error: Wrong password" << std::endl;
+        return 1;
+    }
     int port = atoi(av[1]);
     if (port < 1024 || port > 65535) {
         std::cerr << "Error: Port must be between 1024 and 65535" << std::endl;
         return 1;
     }
-
     return port;
 }
 
@@ -22,7 +27,7 @@ int main(int ac, char **av){
     if (port == 1)
         exit(1);
     Server serv(port);
-    P << B_G"\n-----WECLOME TO IRC SERVER-----\n" << RESET << E;
+    P << B_G"\n-----WELCOME TO IRC SERVER-----\n" << RESET << E;
     serv.init();
     serv.start();
     serv.stop();
