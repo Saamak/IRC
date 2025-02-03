ecf0f562309eb56c8fa0daceb97f95439d9c6e39
Your Name <you@example.com>
2025-02-03 18:32:24 +0100

    donedd

--- command.hpp
+++ command.hpp
@@ -10,12 +10,24 @@ typedef void(*CommandFunction)(void);
 class command {
 public:
     std::map<std::string, CommandFunction> _cmds;
-    void exec(const std::string &client_data); // Utiliser une référence constante
+    void exec(const std::string &client_data);
     command();
-    static void kick();
-    static void invite();
+    static void nick();
+    static void user();
+    static void join();
+    static void part();
+    static void privmsg();
+    static void notice();
+    static void ping();
+    static void pong();
+    static void quit();
     static void topic();
     static void mode();
+    static void kick();
+    static void invite();
+    static void who();
+    static void whois();
+    static void whowas();
 };
 
 #endif // COMMAND_HPP
--- commands.cpp
+++ commands.cpp
@@ -3,12 +3,40 @@
 #include "colors.h"
 #include <sstream>
 
-void command::kick() {
-    std::cout << "KICK" << std::endl;
+void command::nick() {
+    std::cout << "NICK" << std::endl;
 }
 
-void command::invite() {
-    std::cout << "INVITE" << std::endl;
+void command::user() {
+    std::cout << "USER" << std::endl;
+}
+
+void command::join() {
+    std::cout << "JOIN" << std::endl;
+}
+
+void command::part() {
+    std::cout << "PART" << std::endl;
+}
+
+void command::privmsg() {
+    std::cout << "PRIVMSG" << std::endl;
+}
+
+void command::notice() {
+    std::cout << "NOTICE" << std::endl;
+}
+
+void command::ping() {
+    std::cout << "PING" << std::endl;
+}
+
+void command::pong() {
+    std::cout << "PONG" << std::endl;
+}
+
+void command::quit() {
+    std::cout << "QUIT" << std::endl;
 }
 
 void command::topic() {
@@ -19,11 +47,43 @@ void command::mode() {
     std::cout << "MODE" << std::endl;
 }
 
+void command::kick() {
+    std::cout << "KICK" << std::endl;
+}
+
+void command::invite() {
+    std::cout << "INVITE" << std::endl;
+}
+
+void command::who() {
+    std::cout << "WHO" << std::endl;
+}
+
+void command::whois() {
+    std::cout << "WHOIS" << std::endl;
+}
+
+void command::whowas() {
+    std::cout << "WHOWAS" << std::endl;
+}
+
 command::command() {
-    _cmds["KICK"] = &command::kick;
-    _cmds["INVITE"] = &command::invite;
+    _cmds["NICK"] = &command::nick;
+    _cmds["USER"] = &command::user;
+    _cmds["JOIN"] = &command::join;
+    _cmds["PART"] = &command::part;
+    _cmds["PRIVMSG"] = &command::privmsg;
+    _cmds["NOTICE"] = &command::notice;
+    _cmds["PING"] = &command::ping;
+    _cmds["PONG"] = &command::pong;
+    _cmds["QUIT"] = &command::quit;
     _cmds["TOPIC"] = &command::topic;
     _cmds["MODE"] = &command::mode;
+    _cmds["KICK"] = &command::kick;
+    _cmds["INVITE"] = &command::invite;
+    _cmds["WHO"] = &command::who;
+    _cmds["WHOIS"] = &command::whois;
+    _cmds["WHOWAS"] = &command::whowas;
 }
 
 void command::exec(const std::string &client_data) {
